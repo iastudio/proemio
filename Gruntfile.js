f9b941bcd7dbdaeff8b6caf9b675f9b2733d8ad9
Ivan Rudoy - Work <rudooy@gmail.com>
2014-10-02 13:08:23 +1100

    init

---
+++ Gruntfile.js
@@ -0,0 +1,129 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      compile: {
+        files: [{
+          cwd: 'source/jade',
+          src: ['**/*.jade', '!**/includes/*.jade'],
+          dest: 'dest',
+          expand: true,
+          ext: '.html'
+        }]
+      },
+      options: {
+        pretty: true,
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          require: ['susy', 'breakpoint'],
+          config: 'config.rb'
+        }
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version', '> 1%', 'Firefox ESR', 'Opera 12.1' ]
+      },
+      multiple_files: {
+        expand: true,
+        flatten: true,
+        src: 'source/css/*.css',
+        dest: 'dest/css/'
+      },
+    },
+    copy: {
+      css: {
+        files: [{
+          cwd: 'source/css',
+          src: ['**/*.css'],
+          dest: 'dest/css',
+          expand: true,
+        }]
+      },
+      js: {
+        files: [{
+          cwd: 'source/js',
+          src: ['**/*.js'],
+          dest: 'dest/js',
+          expand: true,
+        }]
+      },
+      img: {
+        files: [{
+          cwd: 'source/i',
+          src: ['**/*'],
+          dest: 'dest/i',
+          expand: true,
+        }]
+      },
+      fonts: {
+        files: [{
+          cwd: 'source/fonts',
+          src: ['**/*'],
+          dest: 'dest/fonts',
+          expand: true,
+        }]
+      }
+    },
+    watch: {
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: ['dest/**/*'],
+      },
+      js: {
+        files: ['source/js/**/*.js'],
+        tasks: ['copy:js'],
+      },
+      css: {
+        files: ['source/css/**/*.css'],
+        tasks: ['copy:css', 'autoprefixer'],
+      },
+      jade: {
+        files: ['source/**/*.jade'],
+        tasks: ['jade'],
+      },
+      compass: {
+        files: 'source/**/*.scss',
+        tasks: ['compass']
+      },
+      img: {
+        files: 'source/i/**/*',
+        tasks: ['copy:img']
+      },
+      fonts: {
+        files: 'source/fonts/**/*',
+        tasks: ['copy:fonts']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          hostname: '127.0.0.1',
+          port: 3003,
+          dest: '.'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', [
+    'connect',
+    'copy',
+    'jade',
+    'compass',
+    'autoprefixer',
+    'watch'
+  ]);
+}
